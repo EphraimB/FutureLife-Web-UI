55480c876dd9b3beeda8f05266297b98a09879db
Ephraim Becker <emb16@outlook.com>
2015-09-02 15:53:49 -0400

    NotificationCenter API is now Out of the box!

--- javascript/FutureLifeWebUX.js
+++
@@ -1,4 +0,0 @@
-// Show/hide Toggle switch
-var NotificationCenter = document.getElementsByClassName("NotificationCenter");
-var Notification = document.getElementsByClassName("Notification");
-
---
+++ js/FutureLifeWebUX.js
@@ -0,0 +1,11 @@
+// Create Out of the box Notification Center
+document.createElement("div").setAttribute("class", "NotificationCenter");
+
+var NotificationCenter = document.getElementsByClassName("NotificationCenter");
+
+NotificationCenter[0].style.right = "0px";
+NotificationCenter[0].style.top = "0px";
+NotificationCenter[0].style.bottom = "0px";
+NotificationCenter[0].style.width = "300px";
+NotificationCenter[0].style.height = "auto";
+NotificationCenter[0].style.background-color = "gray";
